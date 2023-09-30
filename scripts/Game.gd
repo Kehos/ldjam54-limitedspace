0ed740ee8cceeea5cf4c825f218a2c192862ace5
Kehos <pau.kehos@gmail.com>
2023-09-30 20:07:23 +0200

    Fill all rooms with items

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,8 +3,8 @@ extends Node
 var maxIndex = 15
 
 # Items
-var allItems = []
-var currentItems = []
+var allItems: Array[String] = []
+var currentItems: Array[String] = []
 
 # Rooms properties
 var rooms: Array[int] = []
@@ -45,17 +45,28 @@ func get_room_items():
 		currentItems.append(item)
 
 func get_items_on_rooms():
+	# Init items arrays
 	for i in rooms:
 		roomItems.append([])
 		roomKeyItems.append(0)
-		
+	
+	# Add key items to max room
 	for i in range(0, currentItems.size()):
 		var randomIndex = rng.randi_range(0, i)
-		while check_cant_locate(randomIndex):
+		while check_cant_locate(randomIndex, true):
 			randomIndex = rng.randi_range(0, i)
-		
 		roomItems[randomIndex].append(currentItems[i])
 		roomKeyItems[randomIndex] += 1
 		
-func check_cant_locate(index):
-	return (roomItems[index].size() == maxItemPerRoom) or (roomKeyItems[index] == maxKeyItemPerRoom)
+	# Add remaining items to fill rooms
+	for i in range(0, allItems.size()):
+		var randomIndex = rng.randi_range(0, roomItems.size() - 1)
+		while check_cant_locate(randomIndex):
+			randomIndex = rng.randi_range(0, roomItems.size() - 1)
+		roomItems[randomIndex].append(allItems[i])
+		
+func check_cant_locate(index, checkKeyItems = false):
+	if checkKeyItems:
+		return (roomItems[index].size() == maxItemPerRoom) or (roomKeyItems[index] == maxKeyItemPerRoom)
+	else:
+		return roomItems[index].size() == maxItemPerRoom
