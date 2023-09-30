575eb70821a069de5928f66199c05ee35a2d7419
Kehos <pau.kehos@gmail.com>
2023-09-30 21:59:08 +0200

    Added logic to advance rooms with UI buttons

--- scripts/Game/Game.gd
+++ scripts/Game/Game.gd
@@ -4,10 +4,14 @@ extends Node
 var roomDoors = []
 var roomItems = []
 var currentRoom = 0
+var maxRoom = 4
 
 # Current room properties
 var currentRoomItems = []
 var currentRoomID = ""
+
+# Player properties
+var itemInHand = "GRENADE"
 
 func _ready():
 	get_dungeon_properties()
@@ -25,5 +29,25 @@ func set_current_room_properties():
 	
 func set_game_UI():
 	$GameUI.set_current_room(currentRoom)
+	$GameUI.update_player_label(itemInHand)
 	$GameUI.set_door_type(currentRoomID)
 	$GameUI.set_button_texts(currentRoomItems)
+	
+func discard_item():
+	itemInHand = ""
+
+func _on_canvas_layer_item_picked(item):
+	if itemInHand:
+		$GameUI.drop_item(itemInHand)
+	itemInHand = item
+
+func _on_open_door_button_pressed():
+	if itemInHand == currentRoomID:
+		discard_item()
+		if currentRoom != maxRoom:
+			$GameUI/NextRoomButton.show()
+
+func _on_next_room_button_pressed():
+	currentRoom += 1
+	set_current_room_properties()
+	$GameUI/NextRoomButton.hide()
