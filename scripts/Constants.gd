53ab5fd53a3fbd95dae7ad4fb5b95920f0760799
Kehos <pau.kehos@gmail.com>
2023-09-30 19:56:40 +0200

    Initial room config

---
+++ scripts/Constants.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var AVAILABLE_ITEMS: Array[String] = [
+	'GOLDEN_KEY',
+	'SHOTGUN',
+	'CROWBAR',
+	'LOCKPICK',
+	'EXPLOSIVE',
+	'SHEARS',
+	'PEM',
+	'ID_CARD',
+	'USB',
+	'ROBOT_HAND',
+	'FILE_TOOL',
+	'FUEL',
+	'FUSE',
+	'CABLE',
+	'MONEY'
+]
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -2,28 +2,60 @@ extends Node
 
 var maxIndex = 15
 
+# Items
+var allItems = []
+var currentItems = []
+
 # Rooms properties
 var rooms: Array[int] = []
 var totalRoomCount = 5
 var maxItemPerRoom = 3
 var maxKeyItemPerRoom = 2
 var roomItems = []
+var roomKeyItems: Array[int] = []
 
 var rng = RandomNumberGenerator.new()
 
 func _ready():
-	get_room_indexes()
-	print(rooms)
+	get_items_from_constants()
+	
+	get_room_items()
+	print(currentItems)
+	
 	get_items_on_rooms()
 	print(roomItems)
+
+func get_items_from_constants():
+	for item in Constants.AVAILABLE_ITEMS:
+		allItems.append(str(item))
 	
-func get_room_indexes():
+func get_room_items():
+	# Get room item indexes
 	for i in range(0, totalRoomCount):
-		var randomIndex = rng.randi_range(0, maxIndex)
+		var randomIndex = rng.randi_range(0, maxIndex - 1)
 		while rooms.has(randomIndex):
-			randomIndex = rng.randi_range(0, maxIndex)
+			randomIndex = rng.randi_range(0, maxIndex - 1)
 		rooms.append(randomIndex)
+		
+	# Get items from room indexes
+	for i in rooms:
+		var item = Constants.AVAILABLE_ITEMS[i]
+		var itemIndex = allItems.find(item)
+		allItems.remove_at(itemIndex)
+		currentItems.append(item)
 
 func get_items_on_rooms():
-	for index in rooms:
+	for i in rooms:
 		roomItems.append([])
+		roomKeyItems.append(0)
+		
+	for i in range(0, currentItems.size()):
+		var randomIndex = rng.randi_range(0, i)
+		while check_cant_locate(randomIndex):
+			randomIndex = rng.randi_range(0, i)
+		
+		roomItems[randomIndex].append(currentItems[i])
+		roomKeyItems[randomIndex] += 1
+		
+func check_cant_locate(index):
+	return (roomItems[index].size() == maxItemPerRoom) or (roomKeyItems[index] == maxKeyItemPerRoom)
