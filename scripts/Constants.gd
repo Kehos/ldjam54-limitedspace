aa4bef654482b412abdb58c138e20e3f8d87c0f4
Kehos <pau.kehos@gmail.com>
2023-10-05 02:00:10 +0200

    Added item sprites

--- scripts/Constants.gd
+++ scripts/Constants.gd
@@ -1,26 +1,26 @@
 extends Node
 
 var AVAILABLE_ITEMS: Array[String] = [
-	"GOLDEN_KEY",
-	"GUN",
-	"CROWBAR",
-	"LOCKPICK",
-	"EXPLOSIVE",
-	"SHEARS",
-	"DOORKNOB",
-	"ID_CARD",
-	"USB",
-	"ROBOT_HAND",
-	"FILE_TOOL",
-	"FUEL",
-	"FUSE",
-	"BATTERY",
-	"MONEY"
+	"goldenkey",
+	"revolver",
+	"crowbar",
+	"lockpick",
+	"explosive",
+	"shears",
+	"doorknob",
+	"idcard",
+	"usb",
+	"robothand",
+	"filetool",
+	"fuel",
+	"fuse",
+	"battery",
+	"money"
 ]
 
 var ITEM_NAMES: Array[String] = [
 	"Golden key",
-	"Gun",
+	"Revolver",
 	"Crowbar",
 	"Lockpick",
 	"Explosive",
@@ -38,7 +38,7 @@ var ITEM_NAMES: Array[String] = [
 
 var ITEM_DESCRIPTION: Array[String] = [
 	"A golden key. How lucky", # GOLDEN_KEY
-	"A simple pistol with only one bullet inside", # GUN
+	"An old revolver with only one bullet inside", # REVOLVER
 	"A crowbar that is near to break", # CROWBAR
 	"A set of one-use lockpicks", # LOCKPICK
 	"Some plastic explosive. Can't detonate by itself", # EXPLOSIVE
@@ -55,13 +55,13 @@ var ITEM_DESCRIPTION: Array[String] = [
 ]
 
 var ITEM_CLUE: Array[String] = [
-	"A golden ticket for the famous Chocolate Factory", # GOLDEN_KEY
-	"There is a target on the wall from a shooting range", # GUN
+	"A poster from some gold-trading business", # GOLDEN_KEY
+	"There is a target on the wall from a shooting range", # REVOLVER
 	"Some broken wooden planks, it appears they have been removed with something", # CROWBAR
 	"You can see an empty case from a robbery movie", # LOCKPICK
 	"An alert sign preventing from explosions", # EXPLOSIVE
 	"An old rusted roll of barbed wire", # SHEARS
-	"An advertising brochure for a locksmith", # DOORKNOB
+	"A ripped page from a furniture catalog, displaying some different doors", # DOORKNOB
 	"An empty identification lanyard", # ID_CARD
 	"The poster of a hackers TV series", # USB
 	"Some pieces that are from a broken toy robot", # ROBOT_HAND
@@ -74,26 +74,26 @@ var ITEM_CLUE: Array[String] = [
 
 var DOOR_TEXT: Array[String] = [
 	"There is a solid golden lock", # GOLDEN_KEY
-	"The door is painted like it has a target on it with a small hole in the middle", # GUN
+	"The door is painted like it has a target on it with a small hole in the middle", # REVOLVER
 	"The door is covered with some solid nailed wooden planks", # CROWBAR
 	"A normal door with a tiny lock. There seems that no key fits in it", # LOCKPICK
 	"Someone prepared some adhesive for an explosive device", # EXPLOSIVE
 	"There is a simple unlocked door, but it is covered by a lot of barbed wire", # SHEARS
-	"That\'s a pretty solid metal door, but it has no doorknob", # DOORKNOB
+	"That's a pretty solid metal door, but it has no doorknob", # DOORKNOB
 	"You can see an electronic lock with a small slot", # ID_CARD
 	"There is a complex protected computer with some pluggable ports", # USB
 	"There is an apparently touch screen with the shape of a hand", # ROBOT_HAND
 	"An archaic prison door with some solid steel bars", # FILE_TOOL
 	"You can see the lid of a tank", # FUEL
-	"There is a small door with an electrical circuit inside. Something is missing", # FUSE
+	"There is a small door on the side, with an electrical circuit inside. Something is missing", # FUSE
 	"There is a computer connected to the door with a spare cable", # BATTERY
 	"Surprisingly, this is an ATM" # MONEY
 ]
 
 var DOOR_ACTION: Array[String] = [
-	"Wasn't that login? Of course the door opens", # GOLDEN_KEY
-	"You walk two steps backwars. After a precise shot, the gun jams and the door opens", # GUN
-	"After removing the last nail, the crowbar breaks and now it is useless", # CROWBAR
+	"Wasn't that logic? Of course the door opens", # GOLDEN_KEY
+	"You walk two steps backwars. After a precise shot, the revolver jams and the door opens", # REVOLVER
+	"After removing the last nail, the crowbar breaks and now it is useless. You're free to continue", # CROWBAR
 	"You spend some time with the lockpicks, but in the end the door opens", # LOCKPICK
 	"You stick the explosive to the adhesive and stay away. There is a surprisingly small explosion and the door opens", # EXPLOSIVE
 	"Carefully you remove the barbed wire, but the shears are now useless. You can continue", # SHEARS
--- scripts/Item/Item.gd
+++ scripts/Item/Item.gd
@@ -1,3 +1,6 @@
 extends Sprite2D
 
 @export var item_index: int
+
+func _ready():
+	print("Item -> ", item_index)
